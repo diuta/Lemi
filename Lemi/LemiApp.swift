97399ef6bf5fa0f4bebc33345859564c523214ff
diuta <dimasptra@gmail.com>
2026-04-29 10:54:28 +0700

    Initial Commit

---
+++ Lemi/LemiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemiApp.swift
+//  Lemi
+//
+//  Created by Dimas Putra Aryawan on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LemiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
