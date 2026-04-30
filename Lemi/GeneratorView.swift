8afd897d4f460d15ba39f31f7c1ed2250cdef55e
Clement Nathanael <clementnat101@gmail.com>
2026-04-30 21:33:08 +0700

    Static View for generation view

---
+++ Lemi/GeneratorView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneratorView.swift
+//  Lemi
+//
+//  Created by Clement N on 30/04/26.
+//
+
+import SwiftUI
+
+struct GeneratorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneratorView()
+}
