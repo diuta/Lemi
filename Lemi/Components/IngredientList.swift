7d64920a271d8fe24156b1e2b2d3943d4a7c7434
Ananda Rachmawati Purwanto <anandarachmawati287@gmail.com>
2026-05-05 13:17:58 +0700

    anuin ingredients list yawww

--- Lemi/Components/IngredientList.swift
+++ Lemi/Components/IngredientList.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct IngredientList: View {
     let entry: RecipeModel
-
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .leading, spacing: 0) {
@@ -22,37 +22,34 @@ struct IngredientList: View {
                 }
             }
         }
-        .padding(.horizontal, 20)
+        //.padding(.horizontal, 20)
     }
 }
 
 struct DefaultIngredientRow: View {
     let ingredient: Ingredient
-
+    
     var body: some View {
         HStack {
-            Text(
-                ingredient.name
-            )
-            .font(Font.AppTheme.ingredientName)
-            .foregroundColor(Color.AppTheme.textPrimary)
-            .padding(.leading, 10)
-
-            Spacer()
-
             Text(
                 ingredient.measurement
             )
             .font(Font.AppTheme.ingredientMeasurement)
             .foregroundColor(Color.AppTheme.textPrimary)
-            .padding(.trailing, 10)
+            .frame(width: 75, alignment: .leading)
+            
+            Text(
+                ingredient.name
+            )
+            .font(Font.AppTheme.ingredientName)
+            .foregroundColor(Color.AppTheme.textPrimary)
+            .multilineTextAlignment(.leading)
+            Spacer()
         }
         .padding(.vertical, 20)
-        .overlay(alignment: .bottom) {
-            Rectangle()
-                .fill(Color.AppTheme.darkBlue.opacity(0.2))
-                .frame(height: 1)
-        }
+        Divider()
+            .overlay(Color.AppTheme.darkBlue.opacity(0.5))
+        
     }
 }
 
@@ -60,62 +57,67 @@ struct AlternativeIngredientRow: View {
     let ingredient: Ingredient
     @State private var dragOffset: CGFloat = 0
     @State private var showingAlternative = false
-
+    
     var body: some View {
         HStack {
-            Text("«")
-                .foregroundColor(Color.AppTheme.textSecondary)
-
             Text(
                 showingAlternative
-                    ? (ingredient.alternative ?? "") : ingredient.name
-            )
-            .font(Font.AppTheme.ingredientName)
-            .foregroundColor(Color.AppTheme.textSecondary)
-            .padding(.leading, 10)
-
-            Spacer()
-
-            Text(
-                showingAlternative
-                    ? (ingredient.alternativeMeasurement ?? "")
-                    : ingredient.measurement
+                ? (ingredient.alternativeMeasurement ?? "")
+                : ingredient.measurement
             )
             .font(Font.AppTheme.ingredientMeasurement)
-            .foregroundColor(Color.AppTheme.textSecondary)
-            .padding(.trailing, 10)
-
-            Text("»")
-                .foregroundColor(Color.AppTheme.textSecondary)
-
-        }
-        .padding(.horizontal, 12)
-        .padding(.vertical, 20)
-        .background(
-            RoundedRectangle(cornerRadius: 12, style: .continuous)
-                .fill(
-                    Color.AppTheme.darkPink.opacity(0.7)
-                )
-        )
-        .padding(.top, 15)
-        .offset(x: dragOffset)
-        .gesture(
-            DragGesture()
-                .onChanged { gesture in
-                    dragOffset = gesture.translation.width
-                }
-                .onEnded { gesture in
-                    if abs(gesture.translation.width) > 80 {
+            .frame(width: 75, alignment: .leading)
+            
+            Text(
+                showingAlternative
+                ? (ingredient.alternative ?? "") : ingredient.name
+            )
+            .font(Font.AppTheme.ingredientName)
+            .multilineTextAlignment(.leading)
+            Spacer()
+            
+            Image(systemName: "arrow.left.arrow.right")
+                .padding(10)
+                .rotation3DEffect(
+                    .degrees(showingAlternative ? 180 : 0),
+                    axis: (x: 0.0, y: 1.0, z: 0.0) // Sumbu Y yang jadi poros putaran
+                )                .contentShape(Rectangle())
+                .onTapGesture {
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.6)) {
                         showingAlternative.toggle()
                     }
-                    withAnimation(.spring(response: 0.3, dampingFraction: 0.6))
-                    {
-                        dragOffset = 0
-                    }
                 }
-        )
+        }
+        .foregroundColor(Color.AppTheme.darkBlue)
+        .padding(.vertical, 10)
+        Divider()
+            .overlay(Color.AppTheme.darkBlue.opacity(0.5))
     }
+    //        .background(
+    //            RoundedRectangle(cornerRadius: 12, style: .continuous)
+    //                .fill(
+    //                    Color.AppTheme.darkPink.opacity(0.7)
+    //                )
+    //        )
+    // .padding(.top, 15)
+    //.offset(x: dragOffset)
+    //        .gesture(
+    //            dragOffset(){
+    //                .onChanged { gesture in
+    //                    dragOffset = gesture.translation.width
+    //                }
+    //                .onEnded { gesture in
+    //                    if abs(gesture.translation.width) > 80 {
+    //                        showingAlternative.toggle()
+    //                    }
+    //                    withAnimation(.spring(response: 0.3, dampingFraction: 0.6))
+    //                    {
+    //                        dragOffset = 0
+    //                    }
+    //                }
+    //        )
 }
+
 
 #Preview {
     IngredientList(
